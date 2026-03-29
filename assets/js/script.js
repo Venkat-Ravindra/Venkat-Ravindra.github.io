c9304e7d3d4a538efa168ff370e2776481dba0a7
ShivaYad007 <shivayad.g07@gmail.com>
2026-03-29 11:56:58 +0530

    updated email service id

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 // ─── CONFIG ───────────────────────────────────────────────
 
     // ─── CONFIG ───────────────────────────────────────────────
-    const EMAILJS_SERVICE_ID  = "service_f32avtm";   // e.g. "service_xxxxxxx"
-    const EMAILJS_TEMPLATE_ID = "template_8lmdzja";  // e.g. "template_xxxxxxx"
-    const EMAILJS_PUBLIC_KEY  = "BUFtI5ZyWy5vA-9ZA";   // e.g. "xxxxxxxxxxxxxxxxxxxx"
+    const EMAILJS_SERVICE_ID  = "service_dhhfkkk";   // e.g. "service_xxxxxxx"
+    const EMAILJS_TEMPLATE_ID = "template_2dtorjs";  // e.g. "template_xxxxxxx"
+    const EMAILJS_PUBLIC_KEY  = "hsiHLPfrl9abM9F8a";   // e.g. "xxxxxxxxxxxxxxxxxxxx"
 
     // ─── THEME TOGGLE ─────────────────────────────────────────
     const themeToggle = document.getElementById('theme-toggle');
